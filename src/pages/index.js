c436bc34ec6d5b82022ceb824c1ff56dbd747a08
benja <benjamin.astudillo.loyer@gmail.com>
2024-01-28 19:19:30 -0300

    build on netlify -v1

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import scrollTo from 'gatsby-plugin-smoothscroll';
 import styled from "styled-components"
 import { NavBar, NavBarMobile } from "./navbar";
 import { StaticImage } from "gatsby-plugin-image";
+import { Helmet } from "react-helmet";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin, faInstagram, faFacebook } from "@fortawesome/free-brands-svg-icons"
@@ -31,9 +32,9 @@ const IndexPage = () => {
         <div className='header-container'>
 
           <h1>the access to the digital era for your business</h1>
-          <div class="header-cta-container">
+          <div className="header-cta-container">
             <p>sign up for a free call to chat with us</p>
-            <button onClick={()=>scrollTo('.booking-section')}>take me there</button>
+            <button className='header-cta-btn' onClick={()=>scrollTo('.booking-section')}>take me there</button>
           </div>
 
           <div className='header-image'>
@@ -44,7 +45,7 @@ const IndexPage = () => {
 
 
       <section className="help-section">
-        <div class="how-container">
+        <div className="how-container">
         
           <h1>How we can benefit your company</h1>
             <p className='how-paragraph-1'>We make websites to help your potential customers find out about your business, and become recurrent.</p>
@@ -74,19 +75,19 @@ const IndexPage = () => {
       
       <section className="booking-section">
         <h2>book a free no pressure call if your interested, and we can see if our services can help you</h2>
-        <div class="calendly-responsive-wrapper">
+        <div className="calendly-responsive-wrapper">
             <Helmet>
               <script src="https://assets.calendly.com/assets/external/widget.js" type="text/javascript" async></script>
             </Helmet>
             <div id="calendly-imbed">
-              <div class="calendly-inline-widget" data-url="https://calendly.com/holdtml-benjamin/reunion-inicial?primary_color=df7b1e" style={{ minWidth: '320px', height: '92dvh', width: '100%', margin: ' 0px 0px 120px 0px', padding: '0px' }}></div>
+              <div className="calendly-inline-widget" data-url="https://calendly.com/holdtml-benjamin/reunion-inicial?primary_color=df7b1e" style={{ minWidth: '320px', height: '92dvh', width: '100%', margin: ' 0px 0px 120px 0px', padding: '0px' }}></div>
             </div>
           </div>
 
       </section>
 
       <footer className="home-footer">
-        <a type="email" href="mailto:benjamin.astudilloloyer@holdtml.com">benjamin.astudilloloyer@holdtml.com</a>
+        <a className='footer-email sm-icon' type="email" target='_blank' href="mailto:benjamin.astudilloloyer@holdtml.com">benjamin.astudilloloyer@holdtml.com</a>
         <div className="social-media-container">
           <a target='_blank' href="https://instagram.com/benjamin_astudilloloyer/" className="instagram sm-icon"><FontAwesomeIcon icon={faInstagram} /></a>
           <a target='_blank' href="https://facebook.com/benjamin.holdtml" className="facebook sm-icon"><FontAwesomeIcon icon={faFacebook} /></a>
